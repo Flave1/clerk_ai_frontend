b01ec72cd7c10238811d55c3b9a923a9da05be22
favour@swiftcoretech.com <favour@swiftcoretech.com>
2025-11-04 18:07:29 +0000

    change backend url

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://98.80.209.139:8000',
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'ws://98.80.209.139:8000',
-    NEXT_PUBLIC_RT_GATEWAY_URL: process.env.NEXT_PUBLIC_RT_GATEWAY_URL || 'http://98.80.209.139:8000',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://3.235.168.161:8000',
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'ws://3.235.168.161:8000',
+    NEXT_PUBLIC_RT_GATEWAY_URL: process.env.NEXT_PUBLIC_RT_GATEWAY_URL || 'http://3.235.168.161:8000',
   },
   async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://98.80.209.139:8000';
+          const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://3.235.168.161:8000';
     return [
       {
         source: '/api/:path*',
